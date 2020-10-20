b2c2001fa103d61109717df7b6e2ccf2a4a8f569
Minh-ChienDUONG <46671487+Minh-ChienDUONG@users.noreply.github.com>
2020-10-20 16:17:27 +0200

    third commit

--- src/test/java/Controller/PerformanceServiceTest.java
+++ src/test/java/Controller/PerformanceServiceTest.java
@@ -84,41 +84,6 @@ class PerformanceServiceTest {
     }
 
     @Test
-    void process_fourMowers(){
-        Mower m1 = new Mower(1, 0, "LFLF", new Position(1, 2, "N"), new Position(1, 2, "N"));
-        Mower m2 = new Mower(2, 0, "FFRFFR", new Position(3, 3, "E"), new Position(3, 3, "E"));
-        Mower m3 = new Mower(3, 0, "FFRRLLR", new Position(4, 1, "S"), new Position(4, 1, "S"));
-        Mower m4 = new Mower(4, 0, "LRRLFFLLR", new Position(2, 5, "E"), new Position(2, 5, "E"));
-        List<Mower> lstMower = Arrays.asList(m1,m2,m3,m4);
-
-        int WIDTH = 5;
-        int LENGTH = 5;
-
-        PerformanceService perform = new PerformanceService();
-        List<Position> lstPositions = perform.process(lstMower,WIDTH, LENGTH);
-
-        Assertions.assertThat(lstPositions.get(0)).isNotNull();
-        Assertions.assertThat(lstPositions.get(0).getX()).isEqualTo(0);
-        Assertions.assertThat(lstPositions.get(0).getY()).isEqualTo(1);
-        Assertions.assertThat(lstPositions.get(0).getOrientation()).isEqualTo("S");
-
-        Assertions.assertThat(lstPositions.get(1)).isNotNull();
-        Assertions.assertThat(lstPositions.get(1).getX()).isEqualTo(5);
-        Assertions.assertThat(lstPositions.get(1).getY()).isEqualTo(1);
-        Assertions.assertThat(lstPositions.get(1).getOrientation()).isEqualTo("W");
-
-        Assertions.assertThat(lstPositions.get(2)).isNotNull();
-        Assertions.assertThat(lstPositions.get(2).getX()).isEqualTo(4);
-        Assertions.assertThat(lstPositions.get(2).getY()).isEqualTo(0);
-        Assertions.assertThat(lstPositions.get(2).getOrientation()).isEqualTo("W");
-
-        Assertions.assertThat(lstPositions.get(3)).isNotNull();
-        Assertions.assertThat(lstPositions.get(3).getX()).isEqualTo(4);
-        Assertions.assertThat(lstPositions.get(3).getY()).isEqualTo(5);
-        Assertions.assertThat(lstPositions.get(3).getOrientation()).isEqualTo("N");
-    }
-
-    @Test
     void process_move_outOfLawnSize(){
         Mower m1 = new Mower(1, 0, "FFFFFFF", new Position(2, 3, "N"), new Position(2, 3, "N"));
         Mower m2 = new Mower(2, 0, "FFFFFFFF", new Position(5, 1, "E"), new Position(5, 1, "E"));
